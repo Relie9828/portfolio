25d53df1b1bdcb0f67b016430f2d78cc2ed46482
Aurelien <aurelien.e@fomosocial.co>
2023-07-06 23:17:38 -0400

    Update to portfolio.

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -13,7 +13,7 @@ export default function Summary ({ toggleModal, goToNext }) {
                             <header className="summary-title-box">
                                 <p className='summary-title'>Summary</p>
                             </header>
-                            <p className='description'>As a native developer, I present simple and beautifully 
+                            <p className='description'>As a UI/UX developer, I present simple and beautifully 
                             minimalistic User Interfaces to your application&mdash;An application that is appealing 
                             to the end user, which will not take away from the experience that makes your app fun and unique.</p>
                             <a href='#'><button className='action'>view content</button></a>
